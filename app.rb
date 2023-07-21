348a71dbf7e811dc0b9cc2cee6741f0ac4479e3b
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-07-21 21:13:50 +0000

    add exchange rates app

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require "sinatra"
+require "http"
+require "json"
 require "sinatra/reloader"
 
+# Retrieve symbols from appropriate API endpoint
+symbols = JSON.parse(HTTP.get('https://api.exchangerate.host/symbols').to_s)["symbols"].keys
+
+#Routes
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @symbols = symbols
+  erb(:home)
+end
+
+get("/:currency") do
+  @source = params.fetch("currency") 
+  @targets = symbols
+  erb(:currency)
+end
+
+get("/:source/:target") do
+  @source, @target = params.fetch("source"), params.fetch("target")
+
+  # Query exchangerate API for current conversion rate
+  @rate = JSON.parse(HTTP.get("https://api.exchangerate.host/convert?from=#{@source}&to=#{@target}"))["info"]["rate"]
+  erb(:conversion)
 end
